31ca81e611f751196c37515d8d56273712bcab72
dancomputer <64235388+dancomputer@users.noreply.github.com>
2023-12-18 15:56:45 +0100

    Runs much faster
    Use numpy arrays and list comprehension instead of for loops with the NetCDF4 file.

--- MakeBioClimVars.py
+++ MakeBioClimVars.py
@@ -1,59 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 17 21:39:48 2023
+Created on Sun Dec 17 14:02:23 2023
 @author: danie
 
-#
-Construct pertinent yearly bio-climatic indicators using daily climatic data. 
-#
+Compute historical yearly growing season GDDs and cumulative precipitation on a 0.5x0.5 grid over Europe using ISMIP2b agro-climatic data.
+
 """
 
+import xarray as xr
 import numpy as np
-import netCDF4 
-import matplotlib.pyplot as plt
-import pandas as pd
-import geopandas as gpd
 import time
-#%%1. Make an empty NetCDF container for the bioclim. variables
-def nc_shell(file_name,variable,units):
-    fn = file_name
-    ds = netCDF4.Dataset(fn, 'w', format='NETCDF4')
-    # def nc_shell(ds)
-    time = ds.createDimension('time', 145)
-    lat = ds.createDimension('lat', 69)
-    lon = ds.createDimension('lon', 104)
-    
-    times = ds.createVariable('time', 'f4', ('time',))
-    lats = ds.createVariable('lat', 'f4', ('lat',))
-    lons = ds.createVariable('lon', 'f4', ('lon',))
-    value = ds.createVariable(variable, 'f4', ('time', 'lat', 'lon',))
-    value.units = units
-    times.units = 'years since 1661-1-1 00:00:00'
-    times[:] = np.arange(200, 344+1, 1)
-    lats[:] = np.arange(36.25, 70.25+0.5, 0.5)
-    lons[:] = np.arange(-11.25,40.25+0.5,0.5)
-    return ds
-#HTC = nc_shell('./Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_HTC_growing_season-soy-noirr.nc4','Hydrothermal_Coeff','mm/Kelvin')
-GDD = nc_shell('./Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_GDD_growing_season-soy-noirr.nc4','Growing_Degree_Days','Kelvin')
-Precip = nc_shell('./Bioclim_vars/pr_cumul_growing_season_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4','Cumul_Precip','mm')
-#%%2. Load in the raw bioclim. variable ingredients
+import cftime
+#%% 1. Loading the Input Variables, the raw agro-clim. data.
+#1a. Write down the file paths.
 precip_path = "Bioclim_vars/pr_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
 tasmax_path = "Bioclim_vars/tasmax_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
 tasmin_path = "Bioclim_vars/tasmin_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
 plant_day_path = "Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_plantday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
 mature_day_path = "Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_matyday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
 
-tasmax = netCDF4.Dataset(tasmax_path)['tasmax']
-tasmin = netCDF4.Dataset(tasmin_path)['tasmin']
-precip = netCDF4.Dataset(precip_path)['pr']
-plant_day = netCDF4.Dataset(plant_day_path)['plantday-soy-noirr']
-mature_day = netCDF4.Dataset(mature_day_path)['matyday-soy-noirr']
+#1b. Load in the files using xarray, and save them as numpy arrays.
+tasmax = xr.open_dataset(tasmax_path,decode_times=False)['tasmax'].values
+tasmin= xr.open_dataset(tasmin_path,decode_times=False)['tasmin'].values
+precip= xr.open_dataset(precip_path,decode_times=False)['pr'].values
+plant_day = xr.open_dataset(plant_day_path,decode_times=False)['plantday-soy-noirr'].values
+mature_day = xr.open_dataset(mature_day_path,decode_times=False)['matyday-soy-noirr'].values
 
-lat=netCDF4.Dataset(tasmax_path)['lat']
-lon=netCDF4.Dataset(tasmax_path)['lon']
+#%% 2. Creating files for holding the output variables.   The variables are GDDs and cum. precip, the files will have the same dimensions as the input data which are yearly.
+GDD_frame = xr.open_dataset(plant_day_path,decode_times=False)
+GDD_frame = GDD_frame.assign(GDD=GDD_frame["plantday-soy-noirr"]*0)
+GDD_frame = GDD_frame.drop_vars('plantday-soy-noirr')
 
-#%%3. Make Growing Degree Days and cum. precipitation in a for loop (takes roughly 2.6 hours)
+Precip_frame = xr.open_dataset(plant_day_path,decode_times=False)
+Precip_frame = Precip_frame.assign(pre=Precip_frame["plantday-soy-noirr"]*0)
+Precip_frame = Precip_frame.drop_vars('plantday-soy-noirr')
 
+#%% 3. Compute the output variables
+#3a. Define useful functions for computing growing season GDDs & Cumul. Precip. 
 def CheckLeap(Year):  
   # Checking if the given year is leap year  
   if((Year % 400 == 0) or  
@@ -62,7 +45,6 @@ def CheckLeap(Year):
     return True;
   else:
     return False;  
-
 def DaysBefore(Year):  
   relative_to_year = 1861
   days = 0
@@ -74,39 +56,31 @@ def DaysBefore(Year):
               days = days + 365
   return days
 
-              
-start=time.time()
-for i in np.arange(0,len(lat)):
-    for j in np.arange(0,len(lon)):
-        for year in np.arange(0,len(plant_day)):
-            if plant_day[year,i,j].mask == False:
-                soy_min = 10;
-                soy_max = 40;
-                planting = int(DaysBefore(year)+plant_day[year,i,j])
-                maturation = int(planting+mature_day[year,i,j])
-                mean_t = (tasmax[planting:maturation,i,j]+tasmin[planting:maturation,i,j])/2
-                P = sum(precip[planting:maturation,i,j])*86400
-                # active_t =sum((mean_t-273.15)[mean_t-273.15>10])*10
-                # if active_t>0:
-                #     H = P/sum((mean_t-273.15)[mean_t-273.15>10])*10
-                # else:
-                #     H = 0
-                G =  sum(np.clip(mean_t-273.15-10,0,soy_max-soy_min))
-                if np.isnan(G):
-                    print('nan!')
-                    print((i,j))
-                GDD['Growing_Degree_Days'][year,i,j]=G
-                Precip['Cumul_Precip'][year,i,j]=P
-                #HTC['Hydrothermal_Coeff'][year,i,j]=H
-            else:
-                GDD['Growing_Degree_Days'][year,i,j] = None
-                Precip['Cumul_Precip'][year,i,j] = None
-                #HTC['Hydrothermal_Coeff'][year,i,j]= None
-    print(i)
-end=time.time()
-GDD.close()
-Precip.close()
-#HTC.close()
+timer_start = time.time()
+#3b. computing GDDs
+#i. Find the lat/lon of unmasked values of GDD
+lats,lons= np.where(np.sum(plant_day,axis=0)>0)
+years = np.arange(0,145,1)
+#ii. Find planting day timeseries at each lat/lon
+yearstart = [DaysBefore(year) for year in years]
+planting = [yearstart+plant_day[:,i,j] for i,j  in zip(lats,lons) ] 
+maturation = [yearstart+mature_day[:,i,j] for i,j  in zip(lats,lons) ] 
+timer_end = time.time()
+#iii. Compute GDD's using list comprehension. -- 
+soy_min = 10;
+soy_max = 40;
+GDD= [[ np.sum(np.clip(
+                   ((tasmax[int(start):int(end),lats[i],lons[i]]+tasmin[int(start):int(end),lats[i],lons[i]])/2)-273.15-10,0,soy_max-soy_min)) 
+                        for start, end in zip(planting[i],maturation[i])  ]
+                          for i in np.arange(0,4248)]
 
-
-
+#3c. Computing growing season cum. precipitation
+precipitation= [[ np.sum(precip[int(start):int(end),lats[i],lons[i]]) for start, end in zip(planting[i],maturation[i])]  for i in np.arange(0,4248)]
+#%% 4. Fill the output files with the output variables.
+for i in np.arange(0,len(lons)):
+    GDD_frame["GDD"][:,lats[i],lons[i]] = GDD[i]
+    Precip_frame["pre"][:,lats[i],lons[i]] = precipitation[i]
+    
+#%% 5. Save the output files.
+GDD_frame.to_netcdf('./Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_GDD_growing_season-soy-noirr.nc4')
+Precip_frame.to_netcdf('./Bioclim_vars/pr_cumul_growing_season_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4')
