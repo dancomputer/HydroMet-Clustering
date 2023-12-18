f1f71c6450d96e0da5278e2e310506acfb655370
dancomputer <64235388+dancomputer@users.noreply.github.com>
2023-12-18 15:54:30 +0100

    Update Cluster.py
    Updated code comments

--- Cluster.py
+++ Cluster.py
@@ -5,55 +5,49 @@ Created on Wed Jun 28 09:43:39 2023
 @author: danie
 
 #
-Regionalize by bio-climatic statistics, then cluster time series within these regions.
+Regionalize by the climatology of the agro-clim variables outputted by MakeBioClimVars.py, then cluster the time series within these regions by euclidean k-means.
 #
 """
-#%%0. Load in data
 
 import numpy as np
 import netCDF4
 import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
-
-#Yield 
 pd.options.mode.chained_assignment = None  # default='warn'
-plant_path = "C:/Users/danie/OneDrive/Desktop/Giannini+Ghil+Chavez+Ohara/gepic_hadgem2-es_ewembi_historical_2005soc_co2_plantday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
-yield_path="C:/Users\danie\OneDrive\Desktop\Giannini+Ghil+Chavez+Ohara\Bioclim_vars\gepic_hadgem2-es_ewembi_historical_2005soc_co2_yield-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4";
-pixel_map_path="C:/Users/danie/OneDrive/Desktop/Giannini+Ghil+Chavez+Ohara/Europe-mai-large-gepic/nhmm-data/pixel-map_gepic_hadgem2-es_ewembi_historical_2005soc_co2_yield-soy-noirr_europe.txt";
+#%%0. Load all input data.
 
-f = netCDF4.Dataset(yield_path)
-soy_yield = f['yield-soy-noirr']
-time=f.variables['time']
-yield_lat=f.variables['lat']
-yield_lon=f.variables['lon']
-
-#bioclim. variables
-GDD_path = "Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_GDD_growing_season-soy-noirr.nc4"
-GDD = netCDF4.Dataset(GDD_path)['Growing_Degree_Days']
+#0a. Write down the file paths.
+GDD_path = "./Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_GDD_growing_season-soy-noirr.nc4"
 Pre_path = './Bioclim_vars/pr_cumul_growing_season_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4'
-Pre = netCDF4.Dataset(Pre_path)['Cumul_Precip']
-
-#raw bioclim. variable ingredients
-precip_path = "Bioclim_vars/pr_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
-tasmax_path = "Bioclim_vars/tasmax_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
-tasmin_path = "Bioclim_vars/tasmin_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
-plant_day_path = "Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_plantday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
-mature_day_path = "Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_matyday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
+yield_path="./Bioclim_vars\gepic_hadgem2-es_ewembi_historical_2005soc_co2_yield-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4";
+precip_path = "./Bioclim_vars/pr_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
+tasmax_path = "./Bioclim_vars/tasmax_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
+tasmin_path = "./Bioclim_vars/tasmin_day_HadGEM2-ES_historical_r1i1p1_EWEMBI_europe_18610101-20051231.nc4"
+plant_day_path = "./Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_plantday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
+mature_day_path = "./Bioclim_vars/gepic_hadgem2-es_ewembi_historical_2005soc_co2_matyday-soy-noirr_lat36.25to70.25lon-11.25to40.25_annual_1861_2005.nc4"
+#0b. Load in the files as netCDF4. We will join them in a pandas dataframe shortly.
 tasmax = netCDF4.Dataset(tasmax_path)['tasmax']
 tasmin = netCDF4.Dataset(tasmin_path)['tasmin']
 precip = netCDF4.Dataset(precip_path)['pr']
 plant_day = netCDF4.Dataset(plant_day_path)['plantday-soy-noirr']
 mature_day = netCDF4.Dataset(mature_day_path)['matyday-soy-noirr']
+GDD = netCDF4.Dataset(GDD_path)['GDD']
+Pre = netCDF4.Dataset(Pre_path)['pre']
+soy_yield = netCDF4.Dataset(yield_path)['yield-soy-noirr']
 
-#%%1. Get data into a structured setting
-#%%1a. Bio-Clim stats
-Bio_Clim_variables=['GDD_mean','Pre_mean','GDD_var','Pre_var','yield_mean','yield_std','yield_max','yield_min']
+yield_lat = netCDF4.Dataset(yield_path)['lat']
+yield_lon =netCDF4.Dataset(yield_path)['lon']
+#%%1. Making a central dataframe, to fill with relevant quantities for each pixel [1:4248].
+
+#1a. Compute long-term averages of the agro-clim variables. 
+#Filling the central dataframe with these quantities.
+Agro_Clim_variables=['GDD_mean','Pre_mean','GDD_var','Pre_var','yield_mean','yield_std','yield_max','yield_min']
 count = 0;
 a = [None]*(69*104)
 b={}
-for i in np.arange(0,len(yield_lat)):
-    for j in np.arange(0,len(yield_lon)):
+for i in np.arange(0,69):
+    for j in np.arange(0,104):
         soy_slice = soy_yield[:,i,j]
         if (soy_slice.mask.sum() == 0) & (GDD[:,i,j].mask.sum()==0):  
             a[count] = (np.double(GDD[:,i,j].mean()),
@@ -70,42 +64,41 @@ for i in np.arange(0,len(yield_lat)):
         else: 
             a[count] = (np.nan,np.nan,np.nan,np.nan,np.nan,np.nan)
             count = count + 1
-    print(i)
-print(count)
-Bio_Clim_Stats=pd.DataFrame(a,columns=Bio_Clim_variables)
+print(count) #monitor loop progress
+Agro_Clim_stats=pd.DataFrame(a,columns=Agro_Clim_variables)
 
-#%%1b. Bio-Clim time series 
-ts_variables=['Yield','pair'] #raw ingerdients: not neccesary, currently. 'Raw_tasmax','Raw_pre','Raw_plantday','Raw_matday'
+#%%1b. Filling the central dataframe with yield time-series + a variable which gives a 2-D enumeration of the lat/lon pairings. 
+ts_variables=['Yield','lat-lon'] 
 count = 0;
 a = [None]*(69*104)
 for i in np.arange(0,len(yield_lat)):
     for j in np.arange(0,len(yield_lon)):
         soy_slice = soy_yield[:,i,j]
-        pair = (i,j)
+        latlon = (i,j)
         if (soy_slice.mask.sum() == 0) & (GDD[:,i,j].mask.sum()==0):  
                 a[count] = (soy_slice.data,
-                            pair,
+                            latlon,
                             count)
                 b.update({count:(i,j)})
                 count = count + 1
         else: 
-            a[count] = (np.nan,pair,count)
+            a[count] = (np.nan,latlon,count)
             count = count + 1
-    print(i)
 print(count)
 Temporal_Variables = pd.DataFrame(a,columns=ts_variables+['count'])
-#%%2. Set up dataframe 
-
+#%%2. Converting the central dataframe into a pandas format.
 xm,ym = np.meshgrid(yield_lon[:],yield_lat[:]);
 data = {'lon':xm.flatten(),'lat':ym.flatten(),}
 coordinates = pd.DataFrame.from_dict(data);
-df = pd.concat([coordinates,Temporal_Variables,Bio_Clim_Stats],axis=1)
+df = pd.concat([coordinates,Temporal_Variables,Agro_Clim_stats],axis=1)
 
-#%%. Spatially plot some chosen mean statistics 
+#%%. Plot: Spatially plot some chosen mean statistics 
 cluster_variables=['GDD_mean','Pre_mean','yield_mean']
 f, axs = plt.subplots(nrows=len(cluster_variables), ncols=1, figsize=(12, 12))
 # Make the axes accessible with single indexing
 axs = axs.flatten()
+titles = ['Growing Degree Days','Precipitation','Yield']
+Units = ['Kelvin','$kg$ x $m^{-2}$','$Tons$ x $Hectare^{-1}$']
 # Start a loop over all the variables of interest
 for i, col in enumerate(cluster_variables):
     # select the axis where the map will go
@@ -115,10 +108,11 @@ for i, col in enumerate(cluster_variables):
     # Remove axis clutter
     ax.set_axis_off()
     # Set the axis title to the name of variable being plotted
-    ax.set_title(col)
+    ax.set_title(titles[i],fontsize=22)
+    ax.get_figure().axes[-1].set_ylabel(Units[i], size=20)
 # Display the figure
 plt.show()
-#%%3. Transform into geopandas set
+#%%3. Transform again the pandas dataframe into a geopandas dataframe.
 from libpysal.weights import Queen, KNN
 import geopandas
 import shapely 
@@ -150,41 +144,28 @@ def get_gdf_with_squares(gdf_with_points, delta_size=0.25):
     return gdf_squares
 
 gdf = get_gdf_with_squares(gdf_points, delta_size=0.25)
-#%%4. Define a distance matrix for regionalization
-from esda.moran import Moran
+#%%4. Define a distance matrix which will mark contiguity of pixels. For later use in regionalization.
 import numpy
 w = Queen.from_dataframe(gdf_points)
-#%%5. Scale the bio-clim statistics using a robust scale. Robust scaling minimizes the effects of outliers.
+#%%5. Scale the agro-clim statistics using a robust scale. Robust scaling minimizes the effects of outliers.
 import sklearn
 from sklearn.preprocessing import robust_scale, scale, minmax_scale
 bioclim_stats = ['GDD_mean','Pre_mean','yield_mean']
 scaled_stats = robust_scale(gdf[bioclim_stats]) #scales each column
 
-#extra 
+#Save the scaled statistics in the dataframe too.
 gdf['GDD_mean_scaled']=robust_scale(gdf['GDD_mean'])
 gdf['Pre_mean_scaled']=robust_scale(gdf['Pre_mean'])
 gdf['yield_mean_scaled']=robust_scale(gdf['yield_mean'])
-#%%6a. Regionalization (Thanks to an inital inspiration by code from Badr Moufad)
-#%%6a.i. Identify good number of regions
+#%%6. Compute regions of the data based on clustering by the scaled agro-clim statistics
+#6a. Identify optimal number of regions using sillouhette scoring
 from scipy.cluster.hierarchy import dendrogram, linkage
-from matplotlib import pyplot as plt
+
+#use a euclidean metric.
 metric = 'euclidean'
 linkage_method = 'ward'
 X = scaled_stats
 Z = linkage(X, metric=metric, method=linkage_method)
-
-#Check the clustering dendrogram -- an additional, manual heurisitic for determing ideal number of regions.
-plt.figure(figsize=(25, 10))
-plt.title('Hierarchical Clustering Dendrogram')
-plt.xlabel('sample index')
-plt.ylabel('distance')
-dn = dendrogram(Z,
-                truncate_mode='lastp',  # show only the last p merged clusters
-                p=24,  # show only the last p merged clusters
-                leaf_rotation=90.,
-                leaf_font_size=12.,
-                show_contracted=True,  # to get a distribution impression in truncated branches
-                )
 
 #Choose the good cluster number, evaluated by their clustering scores. 
 from sklearn.cluster import AgglomerativeClustering
@@ -215,11 +196,11 @@ ax.set_xlabel('# clusters')
 ax.set_ylabel('score')
 plt.show()
 
-### now, choose the number of regions before a big drop off in score.
+#Now, choose the number of regions. A heuristic is to take the # before a big drop off in score.
 best_n_regions = max(arr_score, key=arr_score.get)
 print(best_n_regions)
-best_n_regions=int(input('Set n regions. Calculation gave ' + str(best_n_regions)+ ' as optimal.: '))
-#%%6a.ii Agg. clustering with good number of regions.
+best_n_regions=int(input('Set n regions.'))
+#%%6b. Compute the optimal regionalization, and label the dataframe with these regional labels.
 model = AgglomerativeClustering(n_clusters=best_n_regions,metric=metric,linkage=linkage_method,connectivity=w.sparse)
 model = model.fit(X)
 
@@ -236,51 +217,51 @@ for index,region in enumerate(list(dict(sorted(regional_yields.items(), key=lamb
     gdf.loc[gdf.unsorted_region == region,'region'] = index
 
 gdf.drop('unsorted_region',axis=1)
-###Plotting regions
+#%%Plot: Plotting regions
 fig, ax = plt.subplots(figsize=(8,6))
 # plot map on axis
 countries = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
 countries.plot(color="lightgrey",ax=ax)
 # plot points
-
 cmap = cm.get_cmap('gist_rainbow', best_n_regions)
 color_list = [mcolors.rgb2hex(cmap(i)) for i in range(cmap.N)]
 plot = gdf.plot(x="lon", y="lat", kind="scatter", c='region', colormap=cmap, ax=ax, legend=True)
+colorbar_ax = ax.get_figure().axes[-1] #to get the last axis of the figure, it's the colorbar axes
+colorbar_ax.set_ylabel("Agroclimatic Region", size=20)
+colorbar_ax.tick_params(labelsize=15)
+tick_locs = (np.arange(best_n_regions) + 0.5)*(best_n_regions-1)/best_n_regions
+colorbar_ax.set_yticks(tick_locs)
+colorbar_ax.set_yticklabels(np.arange(best_n_regions))
 # add grid
 ax.set_xlim([min(yield_lon),max(yield_lon)])
 ax.set_ylim([min(yield_lat),max(yield_lat)])
-plt.title("distance: " + metric + "; " + "variables: " + ", ".join(cluster_stats))
-#ax.grid(b=True, alpha=0.5)
-#
+plt.xticks([])
+ax.get_yaxis().set_visible(False)
+ax.set_xlabel('Northern Europe',fontsize=20,labelpad=20)
 a= plt.show()
 
 
-#%%Visualize regional stats
+#%%Plot: Visualizing the regional data distributions
 x,y,z=gdf['GDD_mean'],gdf['Pre_mean'],gdf['yield_mean']
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly as py
 
 gdf['region_str']=gdf['region'].astype(str)
-fig = px.scatter_3d(gdf, x="GDD_mean_scale", y="Pre_mean_scale", z="yield_mean_scale",color="region_str",color_discrete_sequence=color_list,
+fig = px.scatter_3d(gdf, x="GDD_mean_scaled", y="Pre_mean_scaled", z="yield_mean_scaled",color="region_str",color_discrete_sequence=color_list,
                     category_orders={"region_str": list(np.arange(0,9).astype(str))})
 fig.update_traces(marker=dict(size=2))
 fig.show()
-#py.offline.plot(fig, filename = './Plots/v1a_Regions_Yield_3DScatter.html')
-
-#%%7a. Prepare time-series data for clustering 
+#py.offline.plot(fig, filename = './1a_Regions_Yield_3DScatter.html')
+#%%7a. Prepare time-series data for clustering by scaling.
 cluster_ts = ['Yield']
 gdf_scaled_variables = np.ndarray((len(gdf),145,len(cluster_ts)))
-# packed_data = test['scaled_Pre'].copy()
-# for key, d in list(test.items())[1:]:
-#   packed_data = packed_data.join(d, rsuffix=f"_{key}") 
-
 for i in np.arange(0,len(gdf),1):
     for j in np.arange(len(cluster_ts)):
         print(i)
         gdf_scaled_variables[i,:,j]= scale(gdf[cluster_ts[j]].iloc[i][:])
           
-#%%7b.i. Identify good number of clusters
+#%%7b. Compute time-series clusters by k-means. An important parameter here is max_k, which is the highest cluster number which will enter the competition for optimality.
 from matplotlib import pyplot as plt
 from tslearn.clustering import TimeSeriesKMeans, silhouette_score
 
@@ -299,13 +280,13 @@ for region in np.arange(0,best_n_regions,1):
                                    max_iter=50, random_state=10)
           model.fit(X)
           labels = model.labels_
-        
+ 
           # compute score metric
           #m = metrics.calinski_harabasz_score(X, labels)
           #m = metrics.silhouette_score(X, labels)
           m = silhouette_score(X,labels,metric=metric)
           arr_score[k] = m
-        
+        #Plot the range of cluster scores for each region.
         fig = plt.figure()
         ax = fig.add_axes([0,0,1,1])
         ax.bar(list(arr_score.keys()),list(arr_score.values()))
@@ -313,18 +294,17 @@ for region in np.arange(0,best_n_regions,1):
         ax.set_ylabel('score')
         plt.title('Cluster scores for region '+ str(region))
         plt.show()
-        
         ###Here, simply choose the number of clusters with the highest score. We just want to cluster very similar time series, and there is not as much a need to have more regions.
         best_n_clusters = max(arr_score, key=arr_score.get) 
 
-        #6b.ii ts. clustering based off good number.
+        #Compute the final clustering based off the prefered cluster numbers found above.
         model = TimeSeriesKMeans(n_clusters=best_n_clusters, metric=metric,
                                  max_iter=50, random_state=10)
         model.fit(X)
         labels = model.labels_
         gdf['ts_labels'][gdf['region']==region] += labels #clusters of region 1 range from 1000 to 1000 + # of clusters in region 1,  clusters of region 2 ....
         print(str(best_n_clusters)+' clusters.') 
-#%%Plotting clusters
+#%%Plot: Plotting clusters
 fig, ax = plt.subplots(figsize=(8,6))
 # plot map on axis
 countries = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
@@ -335,40 +315,68 @@ cmap = cm.get_cmap('gist_rainbow', len(gdf['ts_labels'].unique()) )
 color_list = [mcolors.rgb2hex(cmap(i)) for i in range(cmap.N)]
 plot = gdf.plot(x="lon", y="lat", kind="scatter", c='ts_labels', colormap=cmap, ax=ax, legend=True)
 # add grid
+colorbar_ax = ax.get_figure().axes[-1] #to get the last axis of the figure, it's the colorbar axes
+colorbar_ax.set_ylabel("Agroclimatic Region", size=20)
+colorbar_ax.tick_params(labelsize=15)
+tick_locs = (np.arange(best_n_regions) + 0.5)*(best_n_regions-1)/best_n_regions
+colorbar_ax.set_yticks(tick_locs)
+colorbar_ax.set_yticklabels(np.arange(best_n_regions))
+# add grid
 ax.set_xlim([min(yield_lon),max(yield_lon)])
 ax.set_ylim([min(yield_lat),max(yield_lat)])
-plt.title("distance: " + metric + "; " + "variables: " + ", ".join(cluster_stats))
+plt.xticks([])
+ax.get_yaxis().set_visible(False)
+ax.set_xlabel('Europe',fontsize=20,labelpad=20)
+
+ax.set_xlim([min(yield_lon),max(yield_lon)])
+ax.set_ylim([min(yield_lat),max(yield_lat)])
+
+plt.title("distance: " + metric + "; " + "variables: " + ", ".join(cluster_ts))
 ax.grid(b=True, alpha=0.5)
 #
 a= plt.show()
-
-
     
-#%%#Plot a sampling of time-series by cluster
-f, axs = plt.subplots(nrows=5, ncols=1, figsize=(12, 12))
+#%%Plot: Plot a sampling of time-series by cluster
+subplot_labels = [['a)'],['b)'],['c)'],['d)'],['e)']];
+f, axs = plt.subplots(nrows=5,ncols=1,figsize=(12, 12))
 # Make the axes accessible with single indexing
 axs = axs.flatten()
+plt.style.use('default')
+
 # Start a loop over all the variables of interest
-for i,region in enumerate(np.arange(2,7)):
+selected_regions = np.arange(1,6)
+for i,region in enumerate(selected_regions):
     # select the axis where the map will go
     ax = axs[i]
     # Plot the map
-    labels= np.arange(region*1000+1+1,region*1000+2+1)
-    colors = cm.get_cmap('gist_rainbow', 5)
-    colors = [mcolors.rgb2hex(colors(m)) for m in range(colors.N)]
+    labels= np.arange(region*1000+4,region*1000+5)
+    cmap = cm.get_cmap('gist_rainbow', best_n_regions)
+    color_list = [mcolors.rgb2hex(cmap(i)) for i in range(cmap.N)]
     for j,label in enumerate(labels):
         for k,series in enumerate(gdf['Yield'][gdf['ts_labels']==label]):
             #print(len(gdf['Yield'][gdf['ts_labels']==label]))
-            ax.plot(series,color=colors[j])
-    ax.legend(['cluster '+str(label) + ': '+str(len(gdf['Yield'][gdf['ts_labels']==label])) + ' series' for label in labels])
+            ax.plot(series,color='black')
+    ax.legend(['Cluster '+str(label) + ': '+str(len(gdf['Yield'][gdf['ts_labels']==label])) + ' series' for label in labels],loc='lower right',fontsize=20,frameon=True)
         #count=count+1
     # Remove axis clutter
     # Set the axis title to the name of variable being plotted
-    ax.set_title("region " + str(region))
+    if i != len(selected_regions)-1:
+        ax.get_xaxis().set_visible(False)
+    ax.set_xlabel('Years',fontsize=25)
+    ax.set_xlim(-1,144)
+    ax.text(0.01, 0.8, 'Region '+str(region),
+    transform = ax.transAxes,fontsize=25, color='black', bbox=dict(facecolor=color_list[region], alpha=0.5))
+    ax.tick_params(axis='x', labelsize=20)
+    ax.tick_params(axis='y', labelsize=20)
+    ax.set_ylim((ax.get_ylim()[0],ax.get_ylim()[1]*1.07))
 # Display the figure
+#f.supylabel('Yield [T/H]')
+axs[2].set_ylabel('Yield [$Tons$ x $Hectare^{-1}$]',fontsize=25)
+f.tight_layout()
+
 plt.show()
 
-#%%# visualize the probability distribution of bio-clim stats by region or by cluster. Currently it is by region.
+#%%#Plot: visualize the probability distribution of bio-clim stats by region or by cluster. Currently it is by region.
 cluster_variables=['yield_mean_scaled','GDD_mean_scaled','Pre_mean_scaled']
 tidy_db = gdf.set_index("region")
 # Keep only variables used for clustering
@@ -387,7 +395,6 @@ tidy_db.head()
 import seaborn
 # Scale fonts to make them more readable
 seaborn.set(font_scale=1.5)
-seaborn.set_palette(color_list)
 # Setup the facets
 facets = seaborn.FacetGrid(
     data=tidy_db,
@@ -396,10 +403,8 @@ facets = seaborn.FacetGrid(
     sharey=False,
     sharex=False,
     aspect=2,
-    col_wrap=3,
+    col_wrap=1,
 )
 # Build the plot from `sns.kdeplot`
 _ = facets.map(seaborn.kdeplot, "Values", shade=True)
    
-
-
